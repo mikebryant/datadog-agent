9253c00ab2f2cc2302438b60524efbac0f50d1e9
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2021-05-09 21:00:27 -0500

    [dogstatsd] capture: actually write tagger state to capture file

--- pkg/dogstatsd/replay/writer.go
+++ pkg/dogstatsd/replay/writer.go
@@ -175,9 +175,17 @@ cleanup:
 			break cleanup
 		}
 	}
+
+	n, err := tc.WriteState()
+	if err != nil {
+		log.Warnf("There was an issue writing the capture state, capture file may be corrupt: %v", err)
+	} else {
+		log.Warnf("Wrote %d bytes for capture tagger state", n)
+	}
+
 }
 
-// StopCapture stops the ongoing capture if ongoine and returns an error, if any.
+// StopCapture stops the ongoing capture if in process and returns an error, if any.
 func (tc *TrafficCaptureWriter) StopCapture() error {
 	tc.Lock()
 	defer tc.Unlock()
@@ -200,13 +208,6 @@ func (tc *TrafficCaptureWriter) StopCapture() error {
 
 	close(tc.shutdown)
 	tc.ongoing = false
-
-	n, err := tc.WriteState()
-	if err != nil {
-		log.Warnf("There was an issue writing the capture state, capture file may be corrupt: %v", err)
-	} else {
-		log.Warnf("Wrote %d bytes for capture tagger state", n)
-	}
 
 	log.Debug("Capture was stopped")
 	return tc.File.Close()
